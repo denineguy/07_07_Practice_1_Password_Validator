e3f67c9cc81624e0ccd614e5763a4a9ab01aa355
Denine Guy <denineguy@Denines-MacBook-Pro.local>
2022-06-22 19:51:04 -0400

    Add password validator

--- app.js
+++ app.js
@@ -10,3 +10,24 @@
 // isValidPassword('89Fjj1nms', 'dogLuvr');  //true
 // isValidPassword('dogLuvr123!', 'dogLuvr') //false
 // isValidPassword('hello1', 'dogLuvr') //false
+
+function isValidPassword(password, username) {
+    if(password.length < 8) {
+        return false;
+    }
+    if(password.indexOf(' ') !== -1 ) {
+        return false;
+    }
+    if(password.indexOf(username) !== -1) {
+        return false;
+    } 
+    return true;
+}
+
+// function isValidPassword(password, username) {
+//     if(password.length < 8 || password.indexOf(' ') !== -1  || password.indexOf(username) !== -1) {
+//         return false;
+//     } return true;
+// }
+
+
